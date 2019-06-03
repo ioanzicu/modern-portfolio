cccc017d959dde8127b6dff1417c865660979ed3
Ioan Zicu <ioan.zicu94@gmail.com>
2019-06-03 08:41:13 +0200

    Add New Pictures and Hover Effects for Projects

--- src/components/Work.js
+++ src/components/Work.js
@@ -7,6 +7,8 @@ import img2 from "../img/projects/project2.jpg";
 import img3 from "../img/projects/project3.jpg";
 import img4 from "../img/projects/project4.jpg";
 import img5 from "../img/projects/project5.jpg";
+import img6 from "../img/projects/project6.jpg";
+import img7 from "../img/projects/project7.jpg";
 
 export default class Work extends Component {
   render() {
@@ -22,124 +24,178 @@ export default class Work extends Component {
 
           <div className="projects">
             <div>
-              <div class="dtl28">
+              <div class="details">
                 <img src={img1} alt="" />
                 <div class="dtl">
-                  <h3>Chatt App</h3>
+                  <h3>Yelp Camp </h3>
                   <p>
-                    Integer sollicitudin lacus libero, in iaculis turpis
-                    tristique sit amet. Nullam augue elit, feugiat nec arcu
-                    eget, sodales feugiat ligula.
+                    Implemented with HTML5, CSS3, Bootstrap 3, NodeJS, API Maps
+                    and MongoDB. Allow to post camp offers around the world.
+                    Full Responsive.
                   </p>
-                  <a href="#">Read More</a>
-                  <a href="https://github.com/ioanzicu?tab=repositories" />
                   <a
-                    href="https://github.com/ioanzicu?tab=repositories"
+                    href="https://infinite-fortress-48527.herokuapp.com/"
                     className="btn-light"
                   >
                     {" "}
                     <i className="fas fa-eye" /> Project{" "}
                   </a>
                   <a
-                    href="https://github.com/ioanzicu?tab=repositories"
+                    href="https://github.com/ioanzicu/yelp-camp"
                     className="btn-dark"
                   >
                     <i className="fab fa-github" /> Github
                   </a>
                 </div>
               </div>
-              <a href="https://github.com/ioanzicu?tab=repositories">
-                {/* <img src={img1} alt="Project" /> */}
-              </a>
-              {/* <a
-                href="https://github.com/ioanzicu?tab=repositories"
-                className="btn-light"
-              >
-                {" "}
-                <i className="fas fa-eye" /> Project{" "}
-              </a>
-              <a
-                href="https://github.com/ioanzicu?tab=repositories"
-                className="btn-dark"
-              >
-                <i className="fab fa-github" /> Github
-              </a> */}
             </div>
 
-            <div className="item">
-              <a href="https://github.com/ioanzicu?tab=repositories">
-                <img src={img2} alt="Project" />
-              </a>
-              <a
-                href="https://github.com/ioanzicu?tab=repositories"
-                className="btn-light"
-              >
-                {" "}
-                <i className="fas fa-eye" /> Project{" "}
-              </a>
-              <a
-                href="https://github.com/ioanzicu?tab=repositories"
-                className="btn-dark"
-              >
-                <i className="fab fa-github" /> Github
-              </a>
+            <div>
+              <div class="details">
+                <img src={img2} alt="" />
+                <div class="dtl">
+                  <h3>Chatt App</h3>
+                  <p>
+                    Implemented with HTML5, CSS3 and NodeJS sockets. Allow
+                    people to have a real time conversation in separated rooms.
+                    Full Responsive.
+                  </p>
+                  <a
+                    href="https://evening-citadel-63052.herokuapp.com/"
+                    className="btn-light"
+                  >
+                    {" "}
+                    <i className="fas fa-eye" /> Project{" "}
+                  </a>
+                  <a
+                    href="https://github.com/ioanzicu/node-chat-app"
+                    className="btn-dark"
+                  >
+                    <i className="fab fa-github" /> Github
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div class="details">
+                <img src={img3} alt="" />
+                <div class="dtl">
+                  <h3>Phones E-Commerce</h3>
+                  <p>
+                    Implemented with ReactJS, Context API and Pypal API for
+                    payment. Allow user to add product to the cart, choose
+                    quantity and pay via Pypal.
+                  </p>
+                  <a
+                    href="https://phoneonline.netlify.com/"
+                    className="btn-light"
+                  >
+                    {" "}
+                    <i className="fas fa-eye" /> Project{" "}
+                  </a>
+                  <a
+                    href="https://github.com/ioanzicu/react-phone-e-commerce"
+                    className="btn-dark"
+                  >
+                    <i className="fab fa-github" /> Github
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div class="details">
+                <img src={img4} alt="" />
+                <div class="dtl">
+                  <h3>Cart App</h3>
+                  <p>
+                    Implemented with React JS, Redux, Redux Thunk (async), HTML5
+                    and CSS3. This application was made as the solution for an
+                    internship position.
+                  </p>
+                  <a
+                    href="https://fathomless-beach-60487.herokuapp.com/"
+                    className="btn-light"
+                  >
+                    {" "}
+                    <i className="fas fa-eye" /> Project{" "}
+                  </a>
+                  <a
+                    href="https://github.com/ioanzicu/redux-cart-app"
+                    className="btn-dark"
+                  >
+                    <i className="fab fa-github" /> Github
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div class="details">
+                <img src={img5} alt="" />
+                <div class="dtl">
+                  <h3>Iro's Bicycle Warehouse</h3>
+                  <p>
+                    A project made using vanilla PHP, MySQL, HTML, CSS. Is a
+                    plaform wich allow to post offers of bicycles.
+                  </p>
+                  <a
+                    href="https://github.com/ioanzicu/bicycle-shop"
+                    className="btn-dark"
+                  >
+                    <i className="fab fa-github" /> Github
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div class="details">
+                <img src={img6} alt="" />
+                <div class="dtl">
+                  <h3>The Steel Chords</h3>
+                  <p>
+                    Implemented with HTML5, CSS3. An informative landing page
+                    for the guitar shop, including futures, demo and prices.
+                    Full Responsive.
+                  </p>
+                  <a
+                    href="https://github.com/ioanzicu/bicycle-shop"
+                    className="btn-dark"
+                  >
+                    <i className="fab fa-github" /> Github
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div class="details">
+                <img src={img7} alt="" />
+                <div class="dtl">
+                  <h3>Survey Form</h3>
+                  <p>
+                    Implemented with HTML5, CSS3. A Survey Form for collecting
+                    data about preference in using tools for web development.
+                    Full Responsive.
+                  </p>
+                  <a
+                    href="https://codepen.io/ioanzicu/full/yRzMLZ"
+                    className="btn-dark"
+                  >
+                    <i className="fab fa-github" /> Github
+                  </a>
+                </div>
+              </div>
             </div>
 
-            <div className="item">
-              <a href="https://github.com/ioanzicu?tab=repositories">
-                <img src={img3} alt="Project" />
-              </a>
-              <a
-                href="https://github.com/ioanzicu?tab=repositories"
-                className="btn-light"
-              >
-                {" "}
-                <i className="fas fa-eye" /> Project{" "}
-              </a>
-              <a
-                href="https://github.com/ioanzicu?tab=repositories"
-                className="btn-dark"
-              >
-                <i className="fab fa-github" /> Github
-              </a>
-            </div>
-
-            <div className="item">
-              <a href="https://github.com/ioanzicu?tab=repositories">
-                <img src={img4} alt="Project" />
-              </a>
-              <a
-                href="https://github.com/ioanzicu?tab=repositories"
-                className="btn-light"
-              >
-                {" "}
-                <i className="fas fa-eye" /> Project{" "}
-              </a>
-              <a
-                href="https://github.com/ioanzicu?tab=repositories"
-                className="btn-dark"
-              >
-                <i className="fab fa-github" /> Github
-              </a>
-            </div>
-
-            <div className="item">
-              <a href="https://github.com/ioanzicu?tab=repositories">
-                <img src={img5} alt="Project" />
-              </a>
-              <a
-                href="https://github.com/ioanzicu?tab=repositories"
-                className="btn-light"
-              >
-                {" "}
-                <i className="fas fa-eye" /> Project{" "}
-              </a>
-              <a
-                href="https://github.com/ioanzicu?tab=repositories"
-                className="btn-dark"
-              >
-                <i className="fab fa-github" /> Github
-              </a>
+            <div>
+              <div class="details">
+                <img src={img7} alt="" />
+                <div class="dtl">
+                  <h3>In Progres</h3>
+                  <p>...</p>
+                  <a href="" className="btn-dark">
+                    <i className="fab fa-github" /> Github
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         </main>
