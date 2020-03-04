d277cd70b08f3cc7744f88b366a74cf9e7cf6269
ioanz <ioan.zicu94@gmail.com>
2020-03-04 21:17:29 +0100

    move achievements to config

--- src/components/pages/Skills/Skills.js
+++ src/components/pages/Skills/Skills.js
@@ -1,46 +1,38 @@
-import React from 'react'
-import Menu from '../Menu/Menu'
-import Footer from '../Footer/Footer'
-import Heading from '../../Heading/Heading'
-import { SkillsCard } from '../../Card/Card'
-import skillsConfig from './config'
-import './Skills.css'
+import React from "react";
+import Menu from "../Menu/Menu";
+import Footer from "../Footer/Footer";
+import Heading from "../../Heading/Heading";
+import { SkillsCard } from "../../Card/Card";
+import { skillsConfig, achievements } from "./config";
+import "./Skills.css";
 
 const Achievements = () => (
-  <div className='achievements'>
-    <div>
-      1.{' '}
-      <a
-        className='achievement-item'
-        href='https://www.freecodecamp.org/certification/fccdeddf4b2-8af4-4000-95ab-864887c0afab/responsive-web-design'>
-        Responsive Web Design
-      </a>
-    </div>
-    <div>
-      2.{' '}
-      <a
-        className='achievement-item'
-        href='https://www.freecodecamp.org/certification/fccdeddf4b2-8af4-4000-95ab-864887c0afab/javascript-algorithms-and-data-structures'>
-        JavaScript Algorithms and Data Structures
-      </a>
-    </div>
+  <div className="achievements">
+    {achievements.map(({ url, title }, index) => (
+      <div>
+        {index + 1}.{" "}
+        <a className="achievement-item" href={url}>
+          {title}
+        </a>
+      </div>
+    ))}
   </div>
-)
+);
 
 const Skills = ({ location }) => {
   return (
     <>
-      <Menu location={location.pathname} className='page' />
+      <Menu location={location.pathname} className="page" />
       <div>
-        <main id='about'>
+        <main id="about">
           <Heading
-            title='My'
-            secondaryTitle='Skills'
-            subtitle='Achievements'
-            subtitleIcon='text-secondary fas fa-medal fa-1x'
+            title="My"
+            secondaryTitle="Skills"
+            subtitle="Achievements"
+            subtitleIcon="text-secondary fas fa-medal fa-1x"
           />
           <Achievements />
-          <div className='about-info'>
+          <div className="about-info">
             {skillsConfig &&
               skillsConfig.map(({ title, icons }, index) => (
                 <SkillsCard title={title} icons={icons} cardNr={index + 1} />
@@ -50,7 +42,7 @@ const Skills = ({ location }) => {
       </div>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
