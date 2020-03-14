bf35f3aebbe263f3f5f5de53bf02f128791dd206
ioanz <ioan.zicu94@gmail.com>
2020-03-14 21:51:48 +0100

    add project

---
+++ src/components/pages/Work/config.js
@@ -0,0 +1,95 @@
+import img1 from "../../../img/projects/project1.jpg";
+import img2 from "../../../img/projects/project2.jpg";
+import img3 from "../../../img/projects/project3.jpg";
+import img4 from "../../../img/projects/project4.jpg";
+import img5 from "../../../img/projects/project5.jpg";
+import img6 from "../../../img/projects/project6.jpg";
+import img7 from "../../../img/projects/project7.jpg";
+import img8 from "../../../img/projects/project8.jpg";
+import img9 from "../../../img/projects/project9.jpg";
+
+const projects = [
+  {
+    img: img1,
+    title: "Agro Info",
+    description:
+      "Display the forecast of the location. Tools: HTML5, CSS3, React Bootstrap, React JS, TypeScript, Firebase Authentication UI.",
+    github: "https://github.com/ioanzicu/agro-info",
+    previewLink: "https://agro-info.netlify.com/",
+    codePen: ""
+  },
+  {
+    img: img2,
+    title: "Yelp Camp",
+    description:
+      "Allows to post camp offers around the world. Full Responsive. Tools: HTML5, CSS3, Bootstrap 3, NodeJS, API Maps and MongoDB.",
+    github: "https://github.com/ioanzicu/yelp-camp",
+    previewLink: "https://worldwide-yelp-camp.herokuapp.com/",
+    codePen: ""
+  },
+  {
+    img: img3,
+    title: "Burger Builder",
+    description:
+      "Allows to build burger and order it. Tools: React, HTML, CSS, React Router, ReactJS, Redux and Firebase.",
+    github: "https://github.com/ioanzicu/react-burger",
+    previewLink: "https://my-burger-ecbd4.firebaseapp.com/checkout",
+    codePen: ""
+  },
+  {
+    img: img4,
+    title: "Chatt App",
+    description:
+      "Allows people to have a real time conversation in separated rooms. Tools: HTML5, CSS3 JavaScript and NodeJS sockets.",
+    github: "https://github.com/ioanzicu/node-chat-app",
+    previewLink: "https://evening-citadel-63052.herokuapp.com/",
+    codePen: ""
+  },
+  {
+    img: img5,
+    title: "Phones E-Commerce",
+    description:
+      "Allows the user to add the product to the cart, choose quantity and pay via Paypal. Tools: ReactJS, Context API and Pypal API for payment.",
+    github: "https://github.com/ioanzicu/react-phone-e-commerce",
+    previewLink: "https://phoneonline.netlify.com/",
+    codePen: ""
+  },
+  {
+    img: img6,
+    title: "Cart App",
+    description:
+      "This application was made as the solution for an internship position. Tools: React JS, Redux, Redux Thunk (async), HTML5 and CSS3.",
+    github: "https://github.com/ioanzicu/redux-cart-app",
+    previewLink: "https://fathomless-beach-60487.herokuapp.com/",
+    codePen: ""
+  },
+  {
+    img: img7,
+    title: "Iro's Bicycle Warehouse",
+    description:
+      "A web platform that allows posting bicycles for selling. Tools: vanilla PHP, MySQL, Bootstrap 4, HTML5, CSS3.",
+    github: "https://github.com/ioanzicu/bicycle-shop",
+    previewLink: "",
+    codePen: ""
+  },
+  {
+    img: img8,
+    title: "The Steel Chords",
+    description:
+      "A responsive, informative landing page for the guitar shop, including futures, demo, and prices. Tools: HTML5, CSS3.",
+    github: "",
+    previewLink: "",
+    codePen: "https://codepen.io/ioanzicu/full/zQerVK"
+  },
+  {
+    img: img9,
+    title: "Survey Form",
+    description:
+      "A responsive Survey Form for collecting data about preference in using tools for web development. Tools HTML5, CSS3.",
+    github: "",
+    previewLink: "",
+    codePen: "https://codepen.io/ioanzicu/full/yRzMLZ"
+  }
+];
+
+export default projects;
